883d6c39f68a3650580756429e9b22b887d4b9bb
bustedbunny <buster1998@mail.ru>
2021-11-09 11:37:35 +0300

    Add project files.

---
+++ Patcher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace AllDogsAreGoodBois
+{
+    public class Patcher
+    {
+        public static readonly List<ThingDef> leather = new List<ThingDef>() { DefOfClass.Leather_Dog, DefOfClass.Leather_Wolf };
+        public static void AllDogs()
+        {
+            foreach(PawnKindDef dog in DefDatabase<PawnKindDef>.AllDefsListForReading)
+            {
+                if (dog.race != null && leather.Contains(dog.race.race.leatherDef))
+                {
+                    dog.canArriveManhunter = false;
+                }
+            }
+        }
+
+        [StaticConstructorOnStartup]
+        public static class StartUp
+        {
+            static StartUp()
+            {
+                AllDogs();
+            }
+        }
+    }
+
+    [RimWorld.DefOf]
+    public static class DefOfClass
+    {
+        public static ThingDef Leather_Dog;
+        public static ThingDef Leather_Wolf;
+
+        static DefOfClass()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(DefOfClass));
+        }
+    }
+}
